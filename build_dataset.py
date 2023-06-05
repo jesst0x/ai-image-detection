f32811fafa0ce5e8f52f7b8e9031cb9808ac76f5
jesstoh <jesstoh23@gmail.com>
2023-06-04 22:50:44 -0700

    refactor

--- build_dataset.py
+++ build_dataset.py
@@ -7,14 +7,15 @@ from tqdm import tqdm
 def resize(filename, output_dir, image_size=64):
   img = Image.open(filename)
   img = img.resize((image_size, image_size), Image.BILINEAR)
-  img.save(os.path.join(output_dir, filename.split('/')[-1]))
+  img.save(os.path.join(output_dir, str(image_size) + 'x' + str(image_size) + '_' + filename.split('/')[-1]))
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset_dir', default='data/raw/stylegan', help='Directory with raw dataset')
+parser.add_argument('--dataset_dir', default='data/raw/real', help='Directory with raw dataset')
 parser.add_argument('--output_dir', default ='data/64x64/stylegan', help='Directory to save resize dataset')
 parser.add_argument('--image_size',
                     default='64',
                     help='Size to resize the image')
+parser.add_argument('--test_only', default='n', help='Is the dataset for testing only? y or n')
 
 
 if __name__ == '__main__':
@@ -23,6 +24,7 @@ if __name__ == '__main__':
 
   # Size to resize image into
   size = int(args.image_size)
+  test_only = True if args.test_only == 'y' else False
 
   data_dir = args.dataset_dir
   images_filenames = [
@@ -32,9 +34,12 @@ if __name__ == '__main__':
   random.seed(40)
   random.shuffle(images_filenames)
 
-  # Split into 70% train, 20% development and 10% test set
-  train_split = int(0.7 * len(images_filenames))
-  dev_split = int(0.9 * len(images_filenames))
+
+  # Split into 70% train, 20% development and 10% test set for training data sets. If dataset is only for testing only, it won't be split.
+  train_split_ratio = 0 if test_only else 0.7
+  dev_split_ratio = 0 if test_only else 0.9
+  train_split = int(train_split_ratio * len(images_filenames))
+  dev_split = int(dev_split_ratio* len(images_filenames))
 
   train_filenames = images_filenames[:train_split]
   dev_filenames = images_filenames[train_split:dev_split]
